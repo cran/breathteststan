c8ba306784f7ed75e75a2480f9a73763eb5c150f
Dieter Menne <dieter.menne@menne-biomed.de>
2024-01-13 17:20:06 +0000

    version 0.8.5

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,5 @@
 library(testthat)
-library(breathteststan)
 Sys.unsetenv("R_TESTS") # https://github.com/r-lib/testthat/issues/603
+options(Ncpus = parallelly::availableCores(omit = 1))
+
 test_check("breathteststan")
